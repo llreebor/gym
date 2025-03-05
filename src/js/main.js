096463c6e30b131f2ac8be3a7509bfc40986174b
llreebor <timoschuk.roman@gmail.com>
2025-03-05 21:10:24 +0200

    add new section

--- src/js/main.js
+++ src/js/main.js
@@ -22,3 +22,32 @@ new Swiper(".testimonials-swiper", {
 		},
 	},
 })
+
+// FAQ Accordion
+function accordion() {
+	const items = document.querySelectorAll(".accordion-item")
+	const triggers = document.querySelectorAll(".accordion-trigger")
+	const contents = document.querySelectorAll(".accordion-content")
+	triggers.forEach((trigger) => {
+		if (items[0].classList.contains("active")) {
+			contents[0].classList.add("active")
+		}
+		trigger.addEventListener("click", () => {
+			const parent = trigger.parentNode
+			if (!parent.classList.contains("active")) {
+				// If you want only one to be open at a time, and others to close - UNCOMMENT the code below.
+				// items.forEach((item, i) => {
+				// 	if (i !== idx && item.classList.contains('active')) {
+				// 		item.classList.remove('active')
+				// 	}
+				// })
+				// Open the current accordion item
+				parent.classList.add("active")
+			} else {
+				// Close the current accordion item
+				parent.classList.remove("active")
+			}
+		})
+	})
+}
+accordion()
